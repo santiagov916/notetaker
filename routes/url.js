011c9518e3e86e6a4bc5f8fa69a43eabcdbbdc71
santiago <72000936+santiagov916@users.noreply.github.com>
2021-12-19 17:33:49 -0800

    readme

--- routes/url.js
+++ routes/url.js
@@ -3,14 +3,13 @@ const fs= require('fs');
 const router = express.Router();
 const path = require('path');
 const notes = require('../data/notes.json');
-const { v1: uuidv1 } = require('uuid');
 
 router.get('/notes', (req,res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 module.exports = router;
